f074a7949e7d39cf874f765b2ea6b681f945f449
Nalinaksh Gaur <nalinaksh@gmail.com>
2023-09-06 13:29:02 +0530

    Update QA_Langchain.py

--- QA_Langchain.py
+++ QA_Langchain.py
@@ -13,55 +13,52 @@ openai.api_key = "xxxx"
 # import dotenv
 # dotenv.load_env()
 
-from langchain.document_loaders import WebBaseLoader
-url = "https://www.globalgreyebooks.com/online-ebooks/paramhansa-yogananda_autobiography-of-a-yogi_complete-text.html"
-loader = WebBaseLoader(url)
-data = loader.load()
+###START To create and download embeddings, execute steps below on a GPU 
+# from langchain.document_loaders import WebBaseLoader
+# url = "https://www.globalgreyebooks.com/online-ebooks/paramhansa-yogananda_autobiography-of-a-yogi_complete-text.html"
+# loader = WebBaseLoader(url)
+# data = loader.load()
 
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 50)
+# all_splits = text_splitter.split_documents(data)
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 50)
-all_splits = text_splitter.split_documents(data)
-
-#To persist embeddings 
-#embedding=OpenAIEmbeddings(openai_api_key=openai.api_key)
-model_name = "BAAI/bge-large-en"
-model_kwargs = {'device': 'cuda'}
-encode_kwargs = {'normalize_embeddings': False}
-embedding = HuggingFaceEmbeddings(
-    model_name=model_name,
-    model_kwargs=model_kwargs,
-    encode_kwargs=encode_kwargs)
-
-### To create and download embeddings for later use on a CPU, execute steps below on a GPU 
 #persist_dir = "HuggingFaceEmbeddings"
+
+# model_name = "BAAI/bge-large-en"
+# model_kwargs = {'device': 'cuda'}
+# encode_kwargs = {'normalize_embeddings': False}
+# embedding = HuggingFaceEmbeddings(
+#     model_name=model_name,
+#     model_kwargs=model_kwargs,
+#     encode_kwargs=encode_kwargs)
+
 #vectorstore = Chroma.from_documents(documents=all_splits, embedding=embedding, persist_directory=persist_dir)
 #vectorstore.persist()
 #!jar cvf HuggingFaceEmbeddings.jar HuggingFaceEmbeddings
 #Download above jar file on your local machine
 ###END
 
-vectorstore = Chroma.from_documents(documents=all_splits, embedding=embedding)
+###START To use the saved embeddings on a CPU
+#Extract saved embeddings, it takes some time, so wait for around 5 minutes
+!jar xvf HuggingFaceEmbeddings.jar
+time.sleep(300)
 
-### To load and use downloaded embeddings on a CPU, execute steps below
-#!jar xvf HuggingFaceEmbeddings.jar
+model_name = "BAAI/bge-large-en"
+model_kwargs = {'device': 'cpu'}
+encode_kwargs = {'normalize_embeddings': False}
+embedding = HuggingFaceEmbeddings(
+    model_name=model_name,
+    model_kwargs=model_kwargs,
+    encode_kwargs=encode_kwargs)
 
-#model_name = "BAAI/bge-large-en"
-#model_kwargs = {'device': 'cpu'} <--IMP!!! device has to be changed to CPU for embeddings to be used on a CPU
-#encode_kwargs = {'normalize_embeddings': False}
-#embedding = HuggingFaceEmbeddings(
-    #model_name=model_name,
-    #model_kwargs=model_kwargs,
-    #encode_kwargs=encode_kwargs)
-
-#vectorstore = None
-#vectorstore = Chroma(embedding_function=embedding, persist_directory=persist_dir)
+persist_dir = "HuggingFaceEmbeddings"
+vectorstore = None
+vectorstore = Chroma(embedding_function=embedding, persist_directory=persist_dir)
 ###END
 
 question = ""
 
 llm = ChatOpenAI(openai_api_key=openai.api_key, model_name="gpt-3.5-turbo", temperature=0)
-#qa_chain = RetrievalQA.from_chain_type(llm,retriever=vectorstore.as_retriever())
-#qa_chain({"query": question})
 
 template = """Use the following pieces of context to answer the question at the end. 
 If you don't know the answer, just say that you don't know, don't try to make up an answer. 
