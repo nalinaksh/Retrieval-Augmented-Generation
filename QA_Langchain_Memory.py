a2196fde8951ccc3c7dbb9eb3b5d58a6413602a7
Nalinaksh Gaur <nalinaksh@gmail.com>
2023-10-19 17:36:18 +0530

    Update QA_Langchain_Memory.py

--- QA_Langchain_Memory.py
+++ QA_Langchain_Memory.py
@@ -89,7 +89,8 @@ question_generator = LLMChain(llm=llm, prompt=CONDENSE_QUESTION_PROMPT)
 
 prompt_template = """Use the following pieces of context to answer the question at the end. \
 If you don't know the answer, just say that you don't know, don't try to make up an answer. \
-Use three sentences maximum and keep the answer as concise as possible.
+Use three sentences maximum and keep the answer as concise as possible. End all of your responses with \
+"Jai Guru 🙏"
 
 {context}
 
