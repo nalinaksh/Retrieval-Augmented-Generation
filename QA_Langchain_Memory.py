2374d255bae418c97337b0f2690e9afa19f2231b
Nalinaksh Gaur <nalinaksh@gmail.com>
2023-11-15 11:51:59 +0530

    Update QA_Langchain_Memory.py

--- QA_Langchain_Memory.py
+++ QA_Langchain_Memory.py
@@ -31,28 +31,10 @@ sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 ###START To create and download embeddings, execute steps below on a GPU 
-# from langchain.document_loaders import WebBaseLoader
-# url = "https://www.globalgreyebooks.com/online-ebooks/paramhansa-yogananda_autobiography-of-a-yogi_complete-text.html"
-# loader = WebBaseLoader(url)
-# data = loader.load()
-
-# text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 50)
-# all_splits = text_splitter.split_documents(data)
-
+#url = "https://www.globalgreyebooks.com/online-ebooks/paramhansa-yogananda_autobiography-of-a-yogi_complete-text.html"
 #persist_dir = "HuggingFaceEmbeddings"
-
-# model_name = "BAAI/bge-large-en-v1.5"
-# model_kwargs = {'device': 'cuda'}
-# encode_kwargs = {'normalize_embeddings': True} # set True to compute cosine similarity
-# embedding = HuggingFaceBgeEmbeddings(
-#     model_name=model_name,
-#     model_kwargs=model_kwargs,
-#     encode_kwargs=encode_kwargs)
-
-#vectorstore = Chroma.from_documents(documents=all_splits, embedding=embedding, persist_directory=persist_dir)
-#vectorstore.persist()
-#use either jar or tar cmd to create archive files for embeddings
-#!jar cvf HuggingFaceEmbeddings.jar HuggingFaceEmbeddings
+#create_embeddings_vectorstore(url, persist_dir)
+#use tar cmd to create archive files for embeddings
 #!tar cvzf HuggingFaceEmbeddings.tar.gz HuggingFaceEmbeddings
 #Download above jar file on your local machine
 ###END
