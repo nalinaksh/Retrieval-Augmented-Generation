296d5d1ac2899f5144f534ba15e82ed37119c587
Nalinaksh Gaur <nalinaksh@gmail.com>
2023-12-29 13:32:39 +0530

    Update QA_Langchain_Memory.py

--- QA_Langchain_Memory.py
+++ QA_Langchain_Memory.py
@@ -73,8 +73,8 @@ question_generator = LLMChain(llm=llm, prompt=CONDENSE_QUESTION_PROMPT)
 
 prompt_template = """Use the following pieces of context to answer the question at the end. \
 The context is from the book - "Autobiography of a Yogi", written by Paramhansa Yogananda. \
-If you could not find the answer from the given context, just say that you don't know the answer. \
-Directly quote the answer from the context, do not generate answer in your own words. \
+Directly quote the sentences/paragraphs from the given context to answer the question, do not try to generate answer in your own words. \
+If you could not find the answer in the given context, just say that you don't know the answer. \
 Don't consult any other external source to look up or make up the answer. \
 
 {context}
